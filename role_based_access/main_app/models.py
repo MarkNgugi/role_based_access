c5fe49bce46d56cb08cc800969fab154c29d7720
mark <mmarkmarkm0@gmail.com>
2024-10-21 00:35:53 +0300

    defined user roles and permissions:

--- role_based_access/main_app/models.py
+++ role_based_access/main_app/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.contrib.auth.models import Group, Permission
 
-# Create your models here.
+def create_groups():
+    # Create groups
+    admin_group, created = Group.objects.get_or_create(name='Admin')
+    hr_manager_group, created = Group.objects.get_or_create(name='HR Manager')
+    manager_group, created = Group.objects.get_or_create(name='Manager')
+    employee_group, created = Group.objects.get_or_create(name='Employee')
+
+    # Admins - all permissions
+    admin_permissions = Permission.objects.all()  # Get all permissions
+    admin_group.permissions.set(admin_permissions)
+
+    # HR Managers - limited permissions
+    hr_manager_permissions = Permission.objects.filter(codename__in=[
+        'view_employee', 'add_employee', 'change_employee', 'delete_employee',
+        'view_leave', 'change_leave', 'view_performance',
+    ])
+    hr_manager_group.permissions.set(hr_manager_permissions)
+
+    # Managers - view-only permissions
+    manager_permissions = Permission.objects.filter(codename__in=[
+        'view_employee', 'view_performance', 'change_leave'
+    ])
+    manager_group.permissions.set(manager_permissions)
+
+    # Employees - self-service permissions
+    employee_permissions = Permission.objects.filter(codename__in=[
+        'view_self_data', 'change_self_data', 'apply_leave'
+    ])
+    employee_group.permissions.set(employee_permissions)
+
