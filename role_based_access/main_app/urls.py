cca7fcd8828a0cde307c2d5028bb59226e5cf345
mark <mmarkmarkm0@gmail.com>
2024-10-21 00:56:57 +0300

    added templates and urls:

--- role_based_access/main_app/urls.py
+++ role_based_access/main_app/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
+from . import views
 
-urlpatterns=[
-
-    
+urlpatterns = [
+    path('dashboard/', views.dashboard, name='dashboard'),
+    path('admin-dashboard/', views.admin_dashboard, name='admin_dashboard'),
+    path('hr-dashboard/', views.hr_dashboard, name='hr_dashboard'),
+    path('manager-dashboard/', views.manager_dashboard, name='manager_dashboard'),
+    path('employee-dashboard/', views.employee_dashboard, name='employee_dashboard'),
 ]
